f13f8b2eae5f872c2b505f82a972a578659c5840
Junedash001 <pjwapple2004@gmail.com>
2026-01-16 16:28:22 +0900

    Update constants.py

--- snsconverter/constants.py
+++ snsconverter/constants.py
@@ -3,8 +3,9 @@ import re
 
 KEY_ENABLED = "enabled"
 DEFAULT_GUILD = {KEY_ENABLED: False}
-INSTA_REGEX_PATTERN = re.compile(r"https://(?:www\.)?(instagram.com)")
 
+INSTA_REGEX_PATTERN = re.compile(r"https://(?:www\.)?(instagram.com)")
+TWITTER_REGEX_PATTERN = re.compile(r"https://(?:www\.)?(twitter\.com|x\.com)")
 
 class SocialMedia(enum.Enum):
     INSTAGRAM = "Instagram"
