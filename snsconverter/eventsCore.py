4d911450f8fb6468199d32db6cd7155f1925b745
Junedash001 <pjwapple2004@gmail.com>
2026-01-16 16:34:03 +0900

    mention

--- snsconverter/eventsCore.py
+++ snsconverter/eventsCore.py
@@ -24,7 +24,8 @@ class EventsCore(Core):
             return
 
         # constructs the message and replies with a mention
-        ok = await message.reply(urls_to_string(ddinsta_urls, SocialMedia.INSTAGRAM))
+        ok = await message.reply(urls_to_string(ddinsta_urls, SocialMedia.INSTAGRAM, message.author.mention))
+
 
         # Remove embeds from user message if reply is successful
         if ok:
