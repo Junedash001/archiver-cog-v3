c58c6d0d6ea9600eca8d9b5a8c4e5e79668f9971
Junedash001 <pjwapple2004@gmail.com>
2026-01-16 16:28:57 +0900

    Update helpers.py

--- snsconverter/helpers.py
+++ snsconverter/helpers.py
@@ -4,6 +4,8 @@ from discord import Embed, Message, channel
 
 from .constants import INSTA_REGEX_PATTERN, SocialMedia
 
+from .constants import INSTA_REGEX_PATTERN, TWITTER_REGEX_PATTERN, SocialMedia
+
 
 def convert_to_ddinsta_url(embeds: list[Embed]):
     """
@@ -37,17 +39,15 @@ def convert_to_vx_twitter_url(embeds: list[Embed]):
     -------
         filtered list of twitter URLs that have been converted to fxtwitter
     """
-    # pulls ALL embeds, not just video embeds
     urls = [entry.url for entry in embeds]
-    vxtwitter_urls = [
+    fxtwitter_urls = []
     for result in urls:
-        if "https://twitter.com" in result:
-            vxtwitter_urls.append(result.replace("https://twitter.com", "https://fxtwitter.com"))
-        elif "https://x.com" in result:
-            vxtwitter_urls.append(result.replace("https://x.com", "https://fxtwitter.com"))
-    ]
-    return vxtwitter_urls
-
+        if re.match(TWITTER_REGEX_PATTERN, result):
+            # Replace both twitter.com and x.com with fxtwitter.com
+            url = re.sub(r"https://(?:www\.)?(twitter\.com|x\.com)", "https://fxtwitter.com", result)
+            fxtwitter_urls.append(url)
+    return fxtwitter_urls
+    
 
 def urls_to_string(links: list[str], socialMedia: SocialMedia):
     """
