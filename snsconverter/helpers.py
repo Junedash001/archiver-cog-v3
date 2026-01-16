868b915b7b585b34ad9c481104ed8badfbad93f9
Junedash001 <pjwapple2004@gmail.com>
2026-01-16 15:56:21 +0900

    Create helpers.py

---
+++ snsconverter/helpers.py
@@ -0,0 +1,99 @@
+import re
+
+from discord import Embed, Message, channel
+
+from .constants import INSTA_REGEX_PATTERN, SocialMedia
+
+
+def convert_to_ddinsta_url(embeds: list[Embed]):
+    """
+    Parameters
+    ----------
+    embeds: list of Discord embeds
+
+    Returns
+    -------
+        filtered list of Instagram URLs that have been converted to ddinstagram
+    """
+
+    # pulls only video embeds from list of embeds
+    urls = [entry.url for entry in embeds]
+
+    ddinsta_urls = [
+        re.sub(INSTA_REGEX_PATTERN, r"https://vx\1", result)
+        for result in urls
+        if re.match(INSTA_REGEX_PATTERN, result)
+    ]
+
+    return ddinsta_urls
+
+
+def convert_to_vx_twitter_url(embeds: list[Embed]):
+    """
+    Parameters
+    ----------
+    embeds: list of Discord embeds
+
+    Returns
+    -------
+        filtered list of twitter URLs that have been converted to vxtwitter
+    """
+
+    # pulls only video embeds from list of embeds
+    urls = [entry.url for entry in embeds if entry.video]
+
+    vxtwitter_urls = [
+        result.replace("https://x.com", "https://fxtwitter.com")
+        for result in urls
+        if "https://x.com" in result
+    ]
+
+    return vxtwitter_urls
+
+
+def urls_to_string(links: list[str], socialMedia: SocialMedia):
+    """
+    Parameters
+    ----------
+    links: list[str]
+        A list of urls
+    socialMedia: SocialMedia
+        The social media to replace.
+
+    Returns
+    -------
+        Formatted output
+    """
+    return "\n".join(
+        [
+            f"your {socialMedia.value} link has been replaced for embeds",
+            *links,
+        ]
+    )
+
+
+def valid(message: Message):
+    """
+    Parameters
+    ----------
+    message: Discord input message object
+
+    Returns
+    -------
+        True if the message is from a human in a guild and contains embeds
+        False otherwise
+    """
+
+    # skips if the message is sent by any bot
+    if message.author.bot:
+        return False
+
+    # skips if message is in dm
+    if isinstance(message.channel, channel.DMChannel):
+        return False
+
+    # skips if the message has no embeds
+    if not message.embeds:
+        return False
+
+    return True
